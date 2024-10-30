378d2639bb2d23e7f74e0c3335c2051c002810dc
huh(pact) <144255472+huhpact@users.noreply.github.com>
2024-10-30 18:07:03 +0100

    30th

--- souhaits/assets/script.js
+++ souhaits/assets/script.js
@@ -28,38 +28,19 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-    // Toggle content and chevron rotation
-    document.getElementById('macbookChevron').addEventListener('click', function() {
-        var details = document.getElementById('macbookDetails');
-        var chevronIcon = this.querySelector('i'); // Find the chevron icon
+    
 
-        // Toggle visibility of the content
-        if (details.style.display === "none" || details.style.display === "") {
-            details.style.display = "block"; // Show the box
-            chevronIcon.className = 'bx bx-chevron-up'; // Change to chevron-up
+
+    document.getElementById('chevronMacBook').addEventListener('click', function() {
+        const reasons = document.getElementById('reasonsMacBook');
+        const chevron = document.getElementById('chevronMacBook');
+        if (reasons.style.display === 'none') {
+            reasons.style.display = 'block';
+            chevron.classList.replace('bx-chevron-down', 'bx-chevron-up');
         } else {
-            details.style.display = "none"; // Hide the box
-            chevronIcon.className = 'bx bx-chevron-down'; // Change to chevron-down
+            reasons.style.display = 'none';
+            chevron.classList.replace('bx-chevron-up', 'bx-chevron-down');
         }
     });
-
-
-    // Open modal when the image is clicked
-    document.getElementById('macbookImage').addEventListener('click', function() {
-        var modal = document.getElementById('imageModal');
-        var modalImg = document.getElementById('modalImage');
-        
-        modal.style.display = "flex"; // Show the modal
-        modalImg.src = this.src; // Set the modal image to the clicked image
-    });
-
-    // Close the modal when clicking anywhere outside the image
-    window.addEventListener('click', function(event) {
-        var modal = document.getElementById('imageModal');
-        if (event.target === modal) {
-            modal.style.display = "none"; // Hide the modal
-        }
-    });
-
-
-
+    
+    
