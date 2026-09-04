b5c5814e26dbb6af6cc65b0ce16a2aefbf0a9df0
huh(pact) <paulchappuzeau@icloud.com>
2026-09-04 22:16:52 +0200

    add

---
+++ quiz/kennzeichen/dvla-region-names.js
@@ -0,0 +1,25 @@
+// Deutsche Anzeigenamen der britischen DVLA-Grossregionen im Kennzeichen-Quiz.
+// Key = "region_key"-Feld aus uk-dvla-regions.geojson, Value = deutscher Anzeigename.
+// ACHTUNG: Grobe Naeherung! DVLA-Regionen entsprechen keinen exakten Verwaltungsgrenzen,
+// siehe PLATES-README.md fuer Details.
+const dvlaRegionNamesDE = {
+  "Anglia": "Anglia (Ostengland)",
+  "Birmingham": "Birmingham",
+  "Cymru": "Cymru (Wales)",
+  "Deeside to Shrewsbury": "Deeside–Shrewsbury",
+  "Essex": "Essex",
+  "Forest and Fens": "Forest & Fens",
+  "Garden of England": "Garden of England (Kent/Sussex)",
+  "Hampshire and Dorset": "Hampshire & Dorset",
+  "London": "London",
+  "Luton/Northampton": "Luton/Northampton",
+  "Manchester and Merseyside": "Manchester & Merseyside",
+  "North": "North (Nordostengland)",
+  "Oxford": "Oxford",
+  "Preston": "Preston/Carlisle",
+  "Reading": "Reading",
+  "Scotland": "Schottland",
+  "Severn Valley": "Severn Valley",
+  "West of England": "West of England",
+  "Yorkshire": "Yorkshire",
+};
---
+++ quiz/kennzeichen/kraj-names.js
@@ -0,0 +1,18 @@
+// Deutsche Anzeigenamen der tschechischen Kraje im Kennzeichen-Quiz.
+// Key = "code"-Feld aus czech-kraje.geojson (SPZ-Kuerzel), Value = deutscher Name.
+const krajNamesDE = {
+  "A": "Prag",
+  "B": "Südmähren",
+  "C": "Südböhmen",
+  "E": "Pardubice",
+  "H": "Königgrätz (Hradec Králové)",
+  "J": "Vysočina (Böhmisch-Mährische Höhe)",
+  "K": "Karlsbad",
+  "L": "Reichenberg (Liberec)",
+  "M": "Olmütz",
+  "P": "Pilsen",
+  "S": "Mittelböhmen",
+  "T": "Mähren-Schlesien",
+  "U": "Aussig (Ústí nad Labem)",
+  "Z": "Zlín",
+};
