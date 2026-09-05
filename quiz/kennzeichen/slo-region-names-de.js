f3f76c38435e416cb186cf0a665dd83e864a41fe
huh(pact) <paulchappuzeau@icloud.com>
2026-09-05 21:56:38 +0200

    add

---
+++ quiz/kennzeichen/slo-region-names-de.js
@@ -0,0 +1,15 @@
+// Deutsche Anzeigenamen der slowenischen Kennzeichen-Regionen im Kennzeichen-Quiz.
+// Key = "code"-Feld aus slovenia-regions.geojson (Kennzeichen-Kuerzel), Value = deutscher Name.
+const sloRegionNamesDE = {
+  "CE": "Cilli (Celje)",
+  "GO": "Görz (Nova Gorica)",
+  "KK": "Krško (Gurkfeld)",
+  "KP": "Koper (Capodistria)",
+  "KR": "Krainburg (Kranj)",
+  "LJ": "Laibach (Ljubljana)",
+  "MB": "Marburg (Maribor)",
+  "MS": "Murska Sobota",
+  "NM": "Rudolfswert (Novo Mesto)",
+  "PO": "Postojna (Adelsberg)",
+  "SG": "Windischgraz (Slovenj Gradec)",
+};
