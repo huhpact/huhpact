99c0a72c48a9e5c18db539138c9bea586db17aad
huh(pact) <paulchappuzeau@icloud.com>
2026-09-06 17:43:32 +0200

    add

---
+++ quiz/kennzeichen/ca-province-names-de.js
@@ -0,0 +1,17 @@
+// Deutsche Anzeigenamen der kanadischen Provinzen/Territorien im Kennzeichen-Quiz.
+// Key = "code"-Feld aus canada-provinces.geojson (Postkuerzel), Value = deutscher Name.
+const caProvinceNamesDE = {
+  "AB": "Alberta",
+  "BC": "British Columbia",
+  "MB": "Manitoba",
+  "NB": "New Brunswick",
+  "NL": "Neufundland und Labrador",
+  "NS": "Nova Scotia",
+  "NT": "Nordwest-Territorien",
+  "NU": "Nunavut",
+  "ON": "Ontario",
+  "PE": "Prince Edward Island",
+  "QC": "Québec",
+  "SK": "Saskatchewan",
+  "YT": "Yukon",
+};
