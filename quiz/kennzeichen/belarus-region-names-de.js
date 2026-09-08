98c645b8fa4215247c8fa8a2c860f8e4476cd8a8
huh(pact) <paulchappuzeau@icloud.com>
2026-09-08 21:54:27 +0200

    add

---
+++ quiz/kennzeichen/belarus-region-names-de.js
@@ -0,0 +1,11 @@
+// Deutsche Anzeigenamen der weissrussischen Regionen im Kennzeichen-Quiz.
+// Key = "code"-Feld aus belarus-regions.geojson (Kennzeichen-Regionsziffer), Value = deutscher Name.
+const belarusRegionNamesDE = {
+  "1": "Brest",
+  "2": "Wizebsk",
+  "3": "Homel",
+  "4": "Hrodna",
+  "5": "Minsk (Gebiet)",
+  "6": "Mahiljou",
+  "7": "Minsk (Stadt)",
+};
